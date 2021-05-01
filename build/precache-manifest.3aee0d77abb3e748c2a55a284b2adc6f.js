58a679c25ed188a20ab9cfaee1dd92bf465cfe9d
João Gomes <joaosilgo96@gmail.com>
2021-05-01 06:53:02 +0100

    🖐

---
+++ build/precache-manifest.3aee0d77abb3e748c2a55a284b2adc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bb28a8d5193b722db7fcac10c8ef83",
+    "url": "/object-detection-app/index.html"
+  },
+  {
+    "revision": "d7626dd1a053da24d3fc",
+    "url": "/object-detection-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0b38a0b95ee1b5396e7a",
+    "url": "/object-detection-app/static/js/2.fc9f5d40.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/object-detection-app/static/js/2.fc9f5d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7626dd1a053da24d3fc",
+    "url": "/object-detection-app/static/js/main.b349c2ca.chunk.js"
+  },
+  {
+    "revision": "fbf93af4f7ac40a59547",
+    "url": "/object-detection-app/static/js/runtime-main.a23dc4a7.js"
+  }
+]);
